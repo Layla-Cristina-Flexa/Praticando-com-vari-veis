b17abd73fa784dc8baa4f57b0176656c8308b503
Laila Cristina <lailacristina377@gmail.com>
2022-06-13 22:11:38 -0300

    Adicionando projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Layla"
+let sobrenome = "Cristina"
+let anoNascimento = "1999"
+let cidade = "Manaus"
+
+console.log(`Eu sou a ${nome} ${sobrenome} tenho ${2022-anoNascimento} anos e moro em ${cidade}`);
